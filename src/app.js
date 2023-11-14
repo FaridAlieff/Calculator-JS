adcb9f12fc622161bcea83f7bead2034bd3c6873
FaridAlieff <farid.alieff@gmail.com>
2023-11-15 01:57:37 +0400

    updated

--- src/app.js
+++ src/app.js
@@ -27,3 +27,40 @@ function calculateSquareRoot() {
 function calculateSquare() {
     resultField.value = Math.pow(eval(resultField.value), 2);
 }
+
+document.addEventListener('keydown', function (event) {
+    handleKeyPress(event.key);
+});
+
+function handleKeyPress(key) {
+    switch (key) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case '.':
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            appendToResult(key);
+            break;
+        case 'Enter':
+            calculateResult();
+            break;
+        case 'Backspace':
+            deleteLast();
+            break;
+        case 'Escape':
+            clearResult();
+            break;
+        default:
+            break;
+    }
+}
